9c26847df9bf01dc352a535f51f637224749ab6f
ptizoom <ptizoom@gmail.com>
2020-01-08 10:46:46 +0100

    h: re-precated obsoleted qVariantValue()

--- modules/qtcore/src/QtCore.h
+++ modules/qtcore/src/QtCore.h
@@ -21,4 +21,35 @@ extern SV* sv_this;
 extern HV* pointer_map;
 extern int do_debug;
 
+
+#if QT_VERSION >= 0x050000 //PTZ200104  in /qt5/QtWidgets/
+#include <QtCore/qvariant.h>
+
+QT_BEGIN_NAMESPACE
+
+
+
+#if QT_DEPRECATED_SINCE(5, 0)
+#warn "using  QT_DEPRECATED T qVariantValue"
+#else
+
+//
+//template<typename T> typedef qVariantFromValue<T> qVariantValue<T>;
+//PTZ200107 could have been qVariantSetValue
+//#define qVariantValue qVariantFromValue
+//typedef qVariantFromValue qVariantValue;
+
+
+
+template<typename T>
+inline QT_DEPRECATED T qVariantValue(const QVariant &variant)
+{ return qvariant_cast<T>(variant); }
+#endif
+
+
+QT_END_NAMESPACE
+
+#endif
+
+
 #endif // QT_H
